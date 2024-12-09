ba10bd5b7994fc1c5095fc2e236ae06d854f32ff
88061 <88061@git>
2024-12-09 09:45:37 +0800

    feat: flexible

--- src/style/flexible.ts
+++ src/style/flexible.ts
@@ -1,12 +1,8 @@
 // Flexible 多分辨率适配，待优化
 (function flexible(window, document) {
   const fs = 16; // unocss 1rem = 16px
-  const PCW = 1920; // PC 设计稿宽度
-  const PCG = PCW / fs;
-  const PADW = 1200; // PC <= => H5W  中间过渡宽度
-  const PADWG = PADW / fs;
-  const H5W = 750; // PC -> H5 布局分割断点宽度
-  const H5G = H5W / fs;
+  const DesignWidth = 750; // 设计稿宽度
+  const Grid = DesignWidth / fs;
   const DOM = document.documentElement;
   const BODY = document.body;
 
@@ -23,14 +19,7 @@
   // set 1rem = viewWidth / copies
   function setRemUnit() {
     const W = DOM.clientWidth;
-    let fontSize;
-    if (W <= H5W) {
-      fontSize = W / H5G;
-    } else if (W > H5W && W < PADW) {
-      fontSize = W / PADWG;
-    } else {
-      fontSize = W / PCG;
-    }
+    const fontSize = W / Grid;
     BODY.style.fontSize = fontSize + "px";
     DOM.style.fontSize = fontSize + "px";
   }
